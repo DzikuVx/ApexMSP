da1937f0bc1d874fa351e2822a5eee3f74727323
Paweł Spychalski <pspychalski@outlook.com>
2026-05-03 17:30:23 +0200

    Add INAV flight modes support

--- src/msp.cpp
+++ src/msp.cpp
@@ -181,73 +181,77 @@ bool MSP::command(uint16_t messageID, void *payload, uint16_t size, bool waitACK
   return true;
 }
 
-// map MSP_MODE_xxx to box ids
-// mixed values from cleanflight and inav
-static const uint8_t BOXIDS[30] PROGMEM = {
-    0,  //  0: MSP_MODE_ARM
-    1,  //  1: MSP_MODE_ANGLE
-    2,  //  2: MSP_MODE_HORIZON
-    3,  //  3: MSP_MODE_NAVALTHOLD (cleanflight BARO)
-    5,  //  4: MSP_MODE_MAG
-    6,  //  5: MSP_MODE_HEADFREE
-    7,  //  6: MSP_MODE_HEADADJ
-    8,  //  7: MSP_MODE_CAMSTAB
-    10, //  8: MSP_MODE_NAVRTH (cleanflight GPSHOME)
-    11, //  9: MSP_MODE_NAVPOSHOLD (cleanflight GPSHOLD)
-    12, // 10: MSP_MODE_PASSTHRU
-    13, // 11: MSP_MODE_BEEPERON
-    15, // 12: MSP_MODE_LEDLOW
-    16, // 13: MSP_MODE_LLIGHTS
-    19, // 14: MSP_MODE_OSD
-    20, // 15: MSP_MODE_TELEMETRY
-    21, // 16: MSP_MODE_GTUNE
-    22, // 17: MSP_MODE_SONAR
-    26, // 18: MSP_MODE_BLACKBOX
-    27, // 19: MSP_MODE_FAILSAFE
-    28, // 20: MSP_MODE_NAVWP (cleanflight AIRMODE)
-    29, // 21: MSP_MODE_AIRMODE (cleanflight DISABLE3DSWITCH)
-    30, // 22: MSP_MODE_HOMERESET (cleanflight FPVANGLEMIX)
-    31, // 23: MSP_MODE_GCSNAV (cleanflight BLACKBOXERASE)
-    32, // 24: MSP_MODE_HEADINGLOCK
-    33, // 25: MSP_MODE_SURFACE
-    34, // 26: MSP_MODE_FLAPERON
-    35, // 27: MSP_MODE_TURNASSIST
-    36, // 28: MSP_MODE_NAVLAUNCH
-    37, // 29: MSP_MODE_AUTOTRIM
+const flight_mode_definition_t INAV_MODE_DEFS[] = {
+    {  0,  0, "ARM" },
+    {  1,  1, "ANGLE" },
+    {  2,  2, "HORIZON" },
+    {  3,  3, "NAV ALTHOLD" },
+    {  5,  4, "HEADING HOLD" },
+    {  6,  5, "HEADFREE" },
+    {  7,  6, "HEADADJ" },
+    {  8,  7, "CAMSTAB" },
+    { 10,  8, "NAV RTH" },
+    { 11,  9, "NAV POSHOLD" },
+    { 12, 10, "MANUAL" },
+    { 13, 11, "BEEPER" },
+    { 15, 12, "LEDS OFF" },
+    { 16, 13, "LIGHTS" },
+    { 19, 15, "OSD OFF" },
+    { 20, 16, "TELEMETRY" },
+    { 21, 28, "AUTO TUNE" },
+    { 26, 17, "BLACKBOX" },
+    { 27, 18, "FAILSAFE" },
+    { 28, 19, "NAV WP" },
+    { 29, 20, "AIR MODE" },
+    { 30, 21, "HOME RESET" },
+    { 31, 22, "GCS NAV" },
+    { 32, 39, "FPV ANGLE MIX" },
+    { 33, 24, "SURFACE" },
+    { 34, 25, "FLAPERON" },
+    { 35, 26, "TURN ASSIST" },
+    { 36, 14, "NAV LAUNCH" },
+    { 37, 27, "SERVO AUTOTRIM" },
+    { 39, 29, "CAMERA CONTROL 1" },
+    { 40, 30, "CAMERA CONTROL 2" },
+    { 41, 31, "CAMERA CONTROL 3" },
+    { 42, 32, "OSD ALT 1" },
+    { 43, 33, "OSD ALT 2" },
+    { 44, 34, "OSD ALT 3" },
+    { 45, 35, "NAV COURSE HOLD" },
+    { 46, 36, "MC BRAKING" },
+    { 47, 37, "USER1" },
+    { 48, 38, "USER2" },
+    { 57, 48, "USER3" },
+    { 58, 49, "USER4" },
+    { 49, 40, "LOITER CHANGE" },
+    { 50, 41, "MSP RC OVERRIDE" },
+    { 51, 42, "PREARM" },
+    { 52, 43, "TURTLE" },
+    { 53, 44, "NAV CRUISE" },
+    { 54, 45, "AUTO LEVEL TRIM" },
+    { 55, 46, "WP PLANNER" },
+    { 56, 47, "SOARING" },
+    { 59, 50, "MISSION CHANGE" },
+    { 60, 51, "BEEPER MUTE" },
+    { 61, 52, "MULTI FUNCTION" },
+    { 62, 53, "MIXER PROFILE 2" },
+    { 63, 54, "MIXER TRANSITION" },
+    { 64, 55, "ANGLE HOLD" },
+    { 65, 56, "GIMBAL LEVEL TILT" },
+    { 66, 57, "GIMBAL LEVEL ROLL" },
+    { 67, 58, "GIMBAL CENTER" },
+    { 68, 59, "GIMBAL HEADTRACKER" }
 };
 
-// returns active mode (using MSP_STATUS and MSP_BOXIDS messages)
-// see MSP_MODE_... for bits inside activeModes
-bool MSP::getActiveModes(uint32_t *activeModes)
+const uint8_t INAV_MODE_DEFS_COUNT = sizeof(INAV_MODE_DEFS) / sizeof(INAV_MODE_DEFS[0]);
+
+bool MSP::checkFlightMode(const flight_mode_definition_t *defs, uint8_t defCount, const uint8_t bitmap[8], uint8_t permanentId)
 {
-  // request status ex
-  INAV_msp_status_t status;
-  if (request(INAV_MSP_STATUS, &status, sizeof(status)))
-  {
-    // request permanent ids associated to boxes
-    uint8_t ids[sizeof(BOXIDS)];
-    uint16_t recvSize;
-    if (request(INAV_MSP_BOXIDS, ids, sizeof(ids), &recvSize))
-    {
-      // compose activeModes, converting BOXIDS to bit map (setting 1 if related flag in flightModeFlags is set)
-      *activeModes = 0;
-      for (uint8_t i = 0; i < recvSize; ++i)
-      {
-        if (status.flightModeFlags & (1 << i))
-        {
-          for (uint8_t j = 0; j < sizeof(BOXIDS); ++j)
-          {
-            if (pgm_read_byte(BOXIDS + j) == ids[i])
-            {
-              *activeModes |= 1 << j;
-              break;
-            }
-          }
-        }
-      }
-      return true;
+  for (uint8_t i = 0; i < defCount; i++) {
+    if (defs[i].permanentId == permanentId) {
+      uint8_t bit = defs[i].boxId;
+      return (bitmap[bit >> 3] & (uint8_t)(1u << (bit & 7))) != 0;
     }
   }
-
   return false;
 }
